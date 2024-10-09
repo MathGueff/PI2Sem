11bd4b9ac0fec034130e64df25788312ee49df3f
Adryann <Adryann2019pontes@gmail.com>
2024-10-09 20:44:32 -0300

    Comitando os componentes 'noticias, doencas e responsaveis'

--- SaneaSP/src/app/app.component.ts
+++ SaneaSP/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "./main/main.component";
+import { DoencasComponent } from './doencas/doencas.component';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, MainComponent, DoencasComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
